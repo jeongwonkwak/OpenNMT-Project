4c6899824681a5299ff3198b93b924d2d8284c2c
KDongYoung <team012@localhost.localdomain>
2020-04-20 16:34:50 +0900

    test

--- onmt/translate/__init__.py
+++ onmt/translate/__init__.py
@@ -5,10 +5,7 @@ from onmt.translate.beam_search import BeamSearch, GNMTGlobalScorer
 from onmt.translate.decode_strategy import DecodeStrategy
 from onmt.translate.greedy_search import GreedySearch
 from onmt.translate.penalties import PenaltyBuilder
-from onmt.translate.translation_server import TranslationServer, \
-    ServerModelError
 
 __all__ = ['Translator', 'Translation', 'BeamSearch',
            'GNMTGlobalScorer', 'TranslationBuilder',
-           'PenaltyBuilder', 'TranslationServer', 'ServerModelError',
-           "DecodeStrategy", "GreedySearch"]
+           'PenaltyBuilder', "DecodeStrategy", "GreedySearch"]
